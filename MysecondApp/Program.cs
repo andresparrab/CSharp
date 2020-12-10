786e3c53d65f25673b30cc307e43bc9af1434f31
doctor <andres.parrab@gmail.com>
2020-12-10 16:14:47 +0100

    update2

--- MysecondApp/Program.cs
+++ MysecondApp/Program.cs
@@ -171,30 +171,64 @@ namespace OOP
     }
     class Program
     {
+        public static double FahrToCels(int fahr)
+        {
+            double temp = ((double)fahr - 32) * 5 / 9;
+            return temp;
+        }
 
+        public static double FahrToCels()
+        {
+            double fahr = new Random().Next(150, 191);
+            System.Console.WriteLine("This is the random temperature: " + fahr);
+            double temp = (fahr - 32) * 5 / 9;
+            return temp;
+        }
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Enter age :");
-            int age = int.Parse(Console.ReadLine());
-            int money = 15000;
-
+            double celsius = 0;
+            int fahrenheit = 0;
             do
             {
-                if (money < 20000 | age < 65)
+                System.Console.WriteLine("Enter the temperature in Fahrenheit: ");
+
+                try
                 {
-                    System.Console.WriteLine("You are saving little");
-                    money += 500;
+
+                    fahrenheit = int.Parse(Console.ReadLine());
+                    if (fahrenheit == 0)
+                        celsius = FahrToCels();
+                    else
+                        celsius = FahrToCels(fahrenheit);
+                }
+                catch (System.Exception error)
+                {
+                    System.Console.WriteLine("Error found");
+                    System.Console.WriteLine(error);
+                }
+
+
+
+
+
+                if (celsius <= 73)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    System.Console.WriteLine("Is to cold. Please tur up the temperature a little bit.");
+                    Console.ResetColor();
                 }
                 else
                 {
-                    System.Console.WriteLine("You are saving a lot");
-                    money += 1000;
-                }
-                age++;
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    System.Console.WriteLine("Is to hot. Please turn dow the heat a little bit.");
+                    Console.ResetColor();
 
-            } while (money < 20000 && age < 65);
-            System.Console.WriteLine("You are retired by the age of {0}", age);
-            System.Console.WriteLine("You are saved {0} dollars ", money);
+                }
+
+
+            } while (celsius < 73 || celsius > 77);
+            System.Console.WriteLine("This is an acceptable temperature! ");
+            System.Console.WriteLine("in celsius it would be: {0:f1} °C ", celsius);
             System.Console.WriteLine("press any key to continue....");
             Console.ReadKey();
 
---
+++ assigmentstosend/bastu.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Bastu
+{
+
+
+    class Program
+    {
+        public static double FahrToCels(int fahr)
+        {
+            double temp = ((double)fahr - 32) * 5 / 9;
+            return temp;
+        }
+
+        public static double FahrToCels()
+        {
+            double fahr = new Random().Next(150, 191);
+            System.Console.WriteLine("This is the random temperature: " + fahr);
+            double temp = (fahr - 32) * 5 / 9;
+            return temp;
+        }
+        static void Main(string[] args)
+        {
+            double celsius = 0;
+            int fahrenheit = 0;
+            do
+            {
+                System.Console.WriteLine("Enter the temperature in Fahrenheit: ");
+
+                try
+                {
+
+                    fahrenheit = int.Parse(Console.ReadLine());
+                    if (fahrenheit == 0)
+                        celsius = FahrToCels();
+                    else
+                        celsius = FahrToCels(fahrenheit);
+                }
+                catch (System.Exception error)
+                {
+                    System.Console.WriteLine("Error found");
+                    System.Console.WriteLine(error);
+                }
+
+
+
+
+
+                if (celsius <= 73)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    System.Console.WriteLine("Is to cold. Please tur up the temperature a little bit.");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    System.Console.WriteLine("Is to hot. Please turn dow the heat a little bit.");
+                    Console.ResetColor();
+
+                }
+
+
+            } while (celsius < 73 || celsius > 77);
+            System.Console.WriteLine("This is an acceptable temperature! ");
+            System.Console.WriteLine("in celsius it would be: {0:f1} °C ", celsius);
+            System.Console.WriteLine("press any key to continue....");
+            Console.ReadKey();
+
+        }
+    }
+
+
+}
