1351d569f3f63515202caf649fcd339215fc3694
Andres Parra <andres.parra@adlibris.com>
2021-05-04 15:02:05 +0200

    Async try

---
+++ AsyncExample/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private int CountCharacters()
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader("C:\\Users\\andres.parra\\source\\repos\\AsyncExample\\words.txt"))
+            {
+                string content = reader.ReadToEnd();
+                count = content.Length;
+                Thread.Sleep(5000);
+            }
+            return count;
+        }
+
+        private async void btnProcessFile_Click(object sender, EventArgs e)
+        {
+            Task<int> task = new Task<int>(CountCharacters);
+            task.Start();
+            lblCount.Text = "Processin File..Please wait";
+            int count = await task;
+            lblCount.Text = count.ToString() + "Character in file";
+        }
+    }
+}
