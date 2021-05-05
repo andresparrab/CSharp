a77e9971121a9b887b0fd7ccecfbe36c55e94ef6
Andres Parra <andres.parra@adlibris.com>
2021-05-05 15:51:47 +0200

    test branching

--- AsyncExample2/WPFUserInterface/MainWindow.xaml.cs
+++ AsyncExample2/WPFUserInterface/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace WPFUserInterface
 
             resultsWindow.Text += $"Total execution time: { elapsedMs }";
         }
-        //LOL vad ska man göra nu då igen eller aldrig
+        //Nu kör vi!!
         private async void executeAsync_Click(object sender, RoutedEventArgs e)
         {
             var watch = System.Diagnostics.Stopwatch.StartNew();
