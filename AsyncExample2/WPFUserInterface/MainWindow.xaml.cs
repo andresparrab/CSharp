5f7764b2957e24825be90e2554262e5f55686bae
Andres Parra <andres.parra@adlibris.com>
2021-05-05 14:34:51 +0200

    testin

--- AsyncExample2/WPFUserInterface/MainWindow.xaml.cs
+++ AsyncExample2/WPFUserInterface/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace WPFUserInterface
 
             resultsWindow.Text += $"Total execution time: { elapsedMs }";
         }
-        //LOL
+        //LO
         private async void executeAsync_Click(object sender, RoutedEventArgs e)
         {
             var watch = System.Diagnostics.Stopwatch.StartNew();
